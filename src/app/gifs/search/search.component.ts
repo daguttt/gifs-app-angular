efbfcccc0b67dd10459fdffd38433d557f8b1afa
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-05-03 18:13:59 -0500

    Class 81: @ViewChild - Get references to html objects

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
 })
-export class SearchComponent {}
+export class SearchComponent {
+  @ViewChild('currentSearch') currentSearch!: ElementRef<HTMLInputElement>;
+  searchGifs() {
+    console.log(this.currentSearch.nativeElement.value);
+    this.currentSearch.nativeElement.value = '';
+  }
+}
